b8b4af748931c80555f7fe68552137c2d6ed524e
saleh <ramezani.saleh.workmail2@gmail.com>
2017-01-01 20:36:03 +0330

    I1 & I2 inputs added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+/* vim: set tabstop=4 autoindent: */
+
 #include <iostream>
 
 using namespace std;
 
 int main(int argc,char * argv){
+	uint32_t i1 = 0; //Number of works
+	float i2 = 0.0f; //Concurrency of works range from 0.0 to 1.0
+	cout<<"Enter I1(number of works): ";
+	cin>>i1;
+	cout<<endl;
+	cout<<"Enter 2(Concurreny constant)(0-1): ";
+	cin>>i2;
+	cout<<endl;
 	return 0;
 }
 
