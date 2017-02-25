4a3f6808411c2a94a533e2c51e62b814e4c9651a
saleh <ramezani.saleh.workmail2@gmail.com>
2017-02-26 02:38:20 +0330

    some bug fixed

--- main.cc
+++ main.cc
@@ -58,13 +58,14 @@ void TestComputeLevelsNumber(){
 
 int main(int argc,char * argv){
 	uint32_t lastFileId = 0;
-	std::cout<< "Enter last file index(first file name supposed to be 1):";
+	std::cout<< "Program will search for files with .rcp extensions, from 1.rcp to {INDEX_YOU_SPECIFIED}.rcp file , and append TAO to the end of files, You better to backup your files first if you want to keep original files."<<std::endl;
+	std::cout << "Enter last file index(first file name supposed to be 1):";
 	std::cin >>lastFileId;
 	for(int i=1;i<=lastFileId;i++){
-		RCPSP rcpsp(std::to_string(lastFileId));
+		RCPSP rcpsp(std::to_string(i)+".rcp");
 		float TAO  = rcpsp.ComputeTAO();
 		std::ofstream rcpspFile;
-		rcpspFile.open(std::to_string(lastFileId),std::ios::app);
+		rcpspFile.open(std::to_string(i)+".rcp",std::ios::app);
 		rcpspFile<<std::endl<<"TAO:"<<TAO;
 		rcpspFile.close();
 	}
